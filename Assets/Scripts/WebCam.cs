5e0e8b0ff4d7b71557b6ffdf7573d87a1802ef35
Ekrem Emre <emre@fzi.de>
2018-05-30 10:16:11 +0200

    added project

---
+++ Assets/Scripts/WebCam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCam : MonoBehaviour
+{
+    public RawImage webcamImage;
+
+    private WebCamTexture webCamTexture;
+
+    private IEnumerator Start()
+    {
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam | UserAuthorization.Microphone);
+
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam | UserAuthorization.Microphone))
+        {
+            webCamTexture = new WebCamTexture();
+            webcamImage.texture = webCamTexture;
+            webcamImage.material.mainTexture = webCamTexture;
+
+            webCamTexture.Play();
+        }
+    }
+}
